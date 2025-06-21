20a9bf5eac5eed29f387c494cf4e676e64d7b3f5
Demo <laleshpawar2025@gmail.com>
2025-06-21 10:26:10 +0530

    Link

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = `https://render-hosting-se2b.onrender.com`;
+const url = `https://realtimecodecompileer-1.onrender.com/`;
 const interval = 30000;
 
 function reloadWebsite() {
